d99fed4a94693430b9b20cb150b9050e57a8d90d
teofiln <teofiln@gmail.com>
2021-08-10 20:57:47 -0600

    add package data files in shiny's session so they can be passed between modules and updated

--- R/app_server.R
+++ R/app_server.R
@@ -6,7 +6,12 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   shinyEventLogger::set_logging()
-  # Your application server logic
+
+  # top app level data objects named differently than the package data
+  session$userData$PARAMS <- params
+  session$userData$DATA_DF <- data_df
+  session$userData$NEW_DATA_DF <- new_data_df
+  
   mod_load_data_server("load_data_ui_1")
   mod_EDA_server("EDA_ui_1")
   mod_kmeans_server("kmeans_ui_1")
