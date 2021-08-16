3db2f45627b3d24935df5c9473c75a58c648928a
teofiln <teofiln@gmail.com>
2021-08-16 11:28:55 -0600

    browser to check if r_data is avaialble in radiant scope

--- R/app_server.R
+++ R/app_server.R
@@ -4,7 +4,7 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
+app_server <- function(input, output, session) {
   shinyEventLogger::set_logging()
 
   # top app level data objects named differently than the package data
@@ -12,12 +12,20 @@ app_server <- function( input, output, session ) {
   session$userData$DATA_DF <- data_df
   session$userData$NEW_DATA_DF <- new_data_df
   
+  browser()
+  
   mod_load_data_server("load_data_ui_1")
   mod_EDA_server("EDA_ui_1")
   mod_kmeans_server("kmeans_ui_1")
   mod_cluster_partition_server("cluster_partition_ui_1")
   mod_boot_kmeans_server("boot_kmeans_ui_1")
   mod_pick_k_server("pick_k_ui_1")
-  callModule(mod_model_based_server, "model_based_ui_1")
+  shiny::callModule(mod_model_based_server, "model_based_ui_1")
+  
+  # return(list(
+  #   set_radiant_data = function(value) {
+  #     session$userData$radiant_data <- value
+  #   }
+  # ))
   
 }
